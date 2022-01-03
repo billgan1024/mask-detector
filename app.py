37985066896a96842114766c87611d0931617df0
Bill Gan <billgan12345@gmail.com>
2022-01-02 21:34:57 -0500

    bruh

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
-import tensorflow as tf
 import tensorflow.keras as keras
 from PIL import Image
 from werkzeug.utils import secure_filename
 import numpy as np
+import tensorflow as tf
 
 app = Flask(__name__)
 model = keras.models.load_model("model.h5")
@@ -15,8 +15,10 @@ def home():
     ret = {}
     for name, file in request.files.items():
         # resize image to 180x180x3
-        img = Image.open(file).convert("RGB").resize((180, 180)) 
+        img = Image.open(file).convert("RGB").resize((180, 180))
         img_array = np.array(img)
         # add it to the dictionary to be returned as a json object
         ret[file.filename] = class_names[int(tf.argmax(model.predict(img_array.reshape(1, 180, 180, 3)), axis=1))]
+        del img
+        del img_array
     return ret 
