f95689a0301a5b9d307a2e66ac01cb088fa181ee
Bill Gan <billgan12345@gmail.com>
2022-01-02 21:10:31 -0500

    bruh

--- app.py
+++ app.py
@@ -8,19 +8,15 @@ import numpy as np
 app = Flask(__name__)
 model = keras.models.load_model("model.h5")
 class_names = ['mask_weared_incorrect', 'with_mask', 'without_mask']
-# model.summary()
 
-# rest api that takes in 28x28 images of files and outputs the number it represents from 0-9 
-# works with multiple file uploads
+# this endpoint can work with multiple image uploads
 @app.route("/", methods=["POST"])
 def home():
     ret = {}
     for name, file in request.files.items():
-        # update dictionary with filename + prediction
-        # convert image to greyscale
+        # resize image to 180x180x3
         img = Image.open(file).convert("RGB").resize((180, 180)) 
         img_array = np.array(img)
+        # add it to the dictionary to be returned as a json object
         ret[file.filename] = class_names[int(tf.argmax(model.predict(img_array.reshape(1, 180, 180, 3)), axis=1))]
-        # ret[file.filename] = tf.argmax(model.predict(img_array.reshape(1, 180, 180, 3)), axis=1)
-    # flask converts a dict to a json object automatically 
     return ret 
--- mask-test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import tensorflow.keras as keras
-model = keras.models.load_model("model.h5")
-print(model.summary())
-
-# mask was trained with this encoding:
-['mask_weared_incorrect', 'with_mask', 'without_mask']
